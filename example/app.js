060fe061c02e7a07e438d07cbc3da9cd1e8be037
re91099z <denis.vilela@br.promonlogicalis.com>
2017-01-11 19:08:20 -0200

    Initial commit

---
+++ example/app.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tablar from '../lib/tablar';
+
+import './app.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            object: {
+                name: "John Doe",
+                age: 45,
+                sex: "MALE",
+                email: "john@doe.com"
+            },
+            types: {
+                name: 'string',
+                age: 'number',
+                sex: 'string',
+                email: 'string'
+            }
+        };
+        this._onObjectChange = this._onObjectChange.bind(this);
+        this._onTypeChange = this._onTypeChange.bind(this);
+    }
+    _onObjectChange(key, value) {
+        const object = Object.assign(this.state.object, {[key]: value});
+        this.setState({
+            object,
+            preview: object
+        });
+    }
+    _onTypeChange(key, type) {
+        this.setState({
+            types: Object.assign({}, this.state.types, {[key]: type})
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Tablar
+                    object={this.state.object}
+                    onValueChange={this._onObjectChange}
+                    onTypeChange={this._onTypeChange}
+                    types={this.state.types} />
+                {this.state.preview ? <per>{JSON.stringify(this.state.preview, null, 2)}</per> : null}
+            </div>
+
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
